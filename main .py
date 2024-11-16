8941234c9e858b1d03c238161e7c6ddb5cf402d4
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-11-16 17:45:35 +0530

    add sprite

---
+++ main .py
@@ -0,0 +1,12 @@
+import pygame
+import ranom
+pygame.init()
+SPRITE_COLOR_CHANGE_EVENT= pygame.USERVENT + 1
+BACKGROUND_COLOR_CHANGE_EVENT= pygame.USERVENT + 2
+pink= pygame.color('pink')
+megenta = pygame.color('megenta')
+darkpink= pygame.color('darkpink')
+blue= pygame.color('blue')
+lightblue= pygame.color('lightblue')
+darkblue= pygame.color('darkblue')
+
